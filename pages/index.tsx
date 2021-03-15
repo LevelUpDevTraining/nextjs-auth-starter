fdf8fb83831e65e9bd0f9f72eb458d892a52a75f
Andrew Bliss <andrewgbliss@gmail.com>
2021-03-15 17:10:23 +0900

    updates

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <h1>Next.js Tailwind CSS Starter</h1>
+    </div>
+  );
+}
